e4440b42ea5ac6a4d0263a5d7ecc3c17e0715c22
Akhilesh v <akhilesh.v2312@gmail.com>
2024-07-02 13:48:23 +0530

    es

---
+++ src/main/java/com/example/osa/util/ErrorStructure.java
@@ -0,0 +1,30 @@
+package com.example.osa.util;
+
+public class ErrorStructure {
+	
+	private int status;
+	private String message;
+	private String rootcause;
+	public int getStatus() {
+		return status;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public String getRootcause() {
+		return rootcause;
+	}
+	public ErrorStructure setStatus(int status) {
+		this.status = status;
+		return this;
+	}
+	public ErrorStructure setMessage(String message) {
+		this.message = message;
+		return this;
+	}
+	public ErrorStructure setRootcause(String rootcause) {
+		this.rootcause = rootcause;
+		return this;
+	}
+
+}
